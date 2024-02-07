c70b17793ee992a0013b76acdc630b9fbfdedabe
LucasFlaquer <lucas.flaquer@gmail.com>
2024-02-06 21:44:29 -0300

    create poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,15 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import z from 'zod';
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello NLW'
+app.post('/polls', async (req, res) => {
+  const createPollBody = z.object({ title: z.string() })
+  const { title } = createPollBody.parse(req.body)
+  const createdPoll = await prisma.poll.create({ data:{ title } })  
+  return res.status(201).send({ pollId: createdPoll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
